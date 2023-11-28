f8628873acbacd7ef33a2f0829dd204ca238bfd9
Denesh Shan <denesh.shan@b4bpayments.com>
2023-11-28 17:10:49 +0000

    Update telescope keymap to search for string under cursor

--- win/nvim/lua/config/keymaps.lua
+++ win/nvim/lua/config/keymaps.lua
@@ -30,6 +30,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
 
 -- NvimTree
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})        -- open/close
