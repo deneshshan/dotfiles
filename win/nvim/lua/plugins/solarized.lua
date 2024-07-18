6016b125ecbf3efae9ef6b63373fe66f7f6a5d55
Denesh Shan <denesh.shan@b4bpayments.com>
2024-07-18 13:22:18 +0100

    Updating vim config

--- win/nvim/lua/plugins/solarized.lua
+++ win/nvim/lua/plugins/solarized.lua
@@ -4,11 +4,26 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
+      colors = {
+        fg = '#fff'
+      }
     },
     init = function()
       vim.o.background = 'dark' -- or 'light'
 
       vim.cmd.colorscheme 'solarized'
+    end,
+    config = function()
+      -- require('solarized').setup {
+      --   highlights = function(colors, colorhelper)
+      --     local lighten = colorhelper.lighten
+
+      --     return {
+      --       Function = { fg = lighten(colors.blue, 40) },
+      --       Normal = { fg = colors.fg }
+      --     }
+      --   end
+      -- }
     end,
   },
 }
--- win/nvim/lua/plugins/tslime.lua
+++ win/nvim/lua/plugins/tslime.lua
@@ -7,9 +7,12 @@ return {
         nmap <C-c><C-c> <Plug>NormalModeSendToTmux
         nmap <C-c><C-r> <Plug>SetTmuxVars
 
-        nmap <Leader>c :call Send_to_Tmux("reset && rails test ".expand('%:~:.')."\n")<CR>
+        nmap <Leader>c :call Send_to_Tmux("reset && rails test -d ".expand('%:~:.')."\n")<CR>
+        nmap <Leader>e :call Send_to_Tmux("exit\n")<CR>
+        nmap <Leader>r :call Send_to_Tmux("!!\n\n")<CR>
         nmap <Leader>s :call Send_to_Tmux("reset && rails test ".expand('%:~:.').":".line('.')."\n")<CR>
-        nmap <Leader>a :call Send_to_Tmux("reset && rails test\n")<CR>
+        nmap <Leader>v :call Send_to_Tmux("reset && rails test -v ".expand('%:~:.').":".line('.')."\n")<CR>
+        nmap <Leader>a :call Send_to_Tmux("reset && rails test -d\n")<CR>
       ]]
     end
   }
