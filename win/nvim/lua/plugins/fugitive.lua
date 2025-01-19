253996c50a5957cc60476e0425aced17b938aa7a
Denesh Shan <denesh.shan@b4bpayments.com>
2025-01-19 19:55:51 +0000

    Add fugitive keymap for git blame

--- win/nvim/lua/plugins/fugitive.lua
+++ win/nvim/lua/plugins/fugitive.lua
@@ -7,6 +7,7 @@ return {
       map("n", "<Leader>gg", "<Cmd>Git<Space>|<Space>res<Space>10<CR>", {})
       map("n", "<Leader>gv", "<Cmd>Gvdiffsplit<CR>", {})
       map("n", "<leader>gl", "<Cmd>0GlLog<CR>", {})
+      map("n", "<leader>gb", "<Cmd>:Git blame<CR>", {})
       vim.cmd([[
         vmap <leader>gp :'<,'>diffput<CR>
       ]])
