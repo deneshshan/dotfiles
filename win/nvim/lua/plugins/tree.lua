26bbcd2a8884d050a6af4f75053b9be2d46e1598
Denesh Shan <denesh.shan@b4bpayments.com>
2025-03-13 11:24:37 +0000

    Add nvim tree shortcuts to resize tree

--- win/nvim/lua/plugins/tree.lua
+++ win/nvim/lua/plugins/tree.lua
@@ -11,13 +11,17 @@
 -- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
 -- disable netrw at the very start of your init.lua (strongly advised)
 return { -- File explore
--- nvim-tree.lua - A file explorer tree for neovim written in lua
-{
+  -- nvim-tree.lua - A file explorer tree for neovim written in lua
+  {
     "nvim-tree/nvim-tree.lua",
     dependencies = {
-        "nvim-tree/nvim-web-devicons",
-        lazy = true,
-        opt = true
+      "nvim-tree/nvim-web-devicons",
+      lazy = true,
+      opt = true
     },
-    config = true
-}}
+    config = true,
+    init = function(_, _)
+      vim.keymap.set("n", "<Leader>nl", ":NvimTreeResize 100<CR>")
+      vim.keymap.set("n", "<Leader>ns", ":NvimTreeResize 30<CR>")
+    end
+  } }
