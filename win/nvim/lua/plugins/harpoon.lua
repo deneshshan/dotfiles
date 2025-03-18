e5dbe2bd771b6cb70fc609e896954199b1afeed9
Denesh Shan <denesh.shan@b4bpayments.com>
2025-03-18 12:11:16 +0000

    Add harpoon plugin to neovim

--- win/nvim/lua/plugins/harpoon.lua
+++ win/nvim/lua/plugins/harpoon.lua
@@ -5,10 +5,11 @@ return {
       local map = vim.api.nvim_set_keymap
 
       map("n", "<Leader>ha", "<Cmd>lua<Space>require(\"harpoon.mark\").add_file()<CR>", {})
+      map("n", "<Leader>hc", "<Cmd>lua<Space>require(\"harpoon.mark\").clear_all()<CR>", {})
       map("n", "<Leader>hq", "<Cmd>lua<Space>require(\"harpoon.ui\").toggle_quick_menu()<CR>", {})
       map("n", "<Leader>hn", "<Cmd>lua<Space>require(\"harpoon.ui\").nav_next()<CR>", {})
       map("n", "<Leader>hp", "<Cmd>lua<Space>require(\"harpoon.ui\").nav_prev()<CR>", {})
-      map("n", "<Leader>ht", "<Cmd>lua<Space>require(\"harpoon.term\").gotoTerminal(1)<CR>", {})
+      map("n", "<Leader>ht", "<Cmd>Telescope<Space>harpoon<Space>marks<CR>", {})
     end
   }
 }
