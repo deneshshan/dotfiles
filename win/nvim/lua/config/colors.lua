c76507f3006bcce169cd503cd228c875073d6f75
Denesh Shan <denesh.shan@gmail.com>
2026-04-29 11:33:22 +0100

    Add colour scheme plugins for nvim

--- win/nvim/lua/config/colors.lua
+++ win/nvim/lua/config/colors.lua
@@ -1,5 +1,5 @@
 vim.cmd([[
-  hi Search gui=bold guifg=#c4b341
+  hi Search gui=bold guifg=#c4b341 guibg=
   hi clear Visual
-  hi Visual gui=bold guifg=#c4b341
+  hi Visual gui=bold guifg=#c4b341 guibg=
 ]])
--- win/nvim/lua/plugins/solarized.lua
+++ win/nvim/lua/plugins/solarized.lua
@@ -6,18 +6,23 @@ return {
     ---@type solarized.config
     opts = {},
     config = function(_, opts)
-      vim.o.termguicolors = true
-      vim.o.background = 'dark'
-      require('solarized').setup(opts)
-      vim.cmd.colorscheme 'solarized'
+      -- vim.o.termguicolors = true
+      -- vim.o.background = 'dark'
+      -- require('solarized').setup(opts)
+      -- vim.cmd.colorscheme 'solarized'
 
-      vim.cmd([[
-        hi clear WinSeparator
-        hi WinSeparator gui=bold guifg=#073642
-        hi clear Type
-        hi Type guifg=#6c71c4
-        hi Parameter guifg=#859900
-      ]])
+      -- vim.api.nvim_set_hl(0, "@keyword.function.ruby", { link = "@type.ruby" })
+      -- -- vim.api.nvim_set_hl(0, "@variable.ruby", { link = "@string.ruby" })
+      -- vim.api.nvim_set_hl(0, "Search", { bg = "NONE", underline = true })
+      -- vim.api.nvim_set_hl(0, "CurSearch", { bg = "NONE", underline = true })
+      -- vim.api.nvim_set_hl(0, "IncSearch", { bg = "NONE", underline = true })
+      -- vim.cmd([[
+      --   hi clear WinSeparator
+      --   hi WinSeparator gui=bold guifg=#073642
+      --   hi clear Type
+      --   hi Type guifg=#6c71c4
+      --   hi Parameter guifg=#859900
+      -- ]])
     end,
   }
 }
