ce5b02b3a9449e33e11cf09b28929450c100b1f6
Denesh Shan <denesh.shan@gmail.com>
2026-05-20 09:56:53 +0100

    Add diffview nvim plugin

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory", "DiffviewToggleFiles", "DiffviewRefresh" },
+    keys = {
+      { "<leader>eh", "<Cmd>DiffviewFileHistory %<CR>", desc = "Diffview: history of current file" },
+      { "<leader>eH", "<Cmd>DiffviewFileHistory<CR>",   desc = "Diffview: history of whole repo" },
+      { "<leader>ed", "<Cmd>DiffviewOpen<CR>",          desc = "Diffview: open working diff" },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        merge_tool = { layout = "diff3_mixed" },
+      },
+    },
+    config = function(_, opts)
+      require("diffview").setup(opts)
+    end,
+  }
+}
