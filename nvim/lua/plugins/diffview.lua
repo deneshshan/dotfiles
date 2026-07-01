f3a3477dfdcd0c02f24b1c8b3a688be0462815db
Denesh Shan <denesh.shan@gmail.com>
2026-07-01 16:01:30 +0100

    Add keymap in diffview to open/edit a changed file

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -12,12 +12,23 @@ return {
       { "<leader>eH", "<Cmd>DiffviewFileHistory<CR>",   desc = "Diffview: history of whole repo" },
       { "<leader>ed", "<Cmd>DiffviewOpen<CR>",          desc = "Diffview: open working diff" },
     },
-    opts = {
-      enhanced_diff_hl = true,
-      view = {
-        merge_tool = { layout = "diff3_mixed" },
-      },
-    },
+    opts = function()
+      local actions = require("diffview.actions")
+      return {
+        enhanced_diff_hl = true,
+        view = {
+          merge_tool = { layout = "diff3_mixed" },
+        },
+        keymaps = {
+          view = {
+            { "n", "<leader>eo", actions.goto_file_edit, { desc = "Diffview: open file (close diffview)" } },
+          },
+          file_panel = {
+            { "n", "<leader>eo", actions.goto_file_edit, { desc = "Diffview: open file (close diffview)" } },
+          },
+        },
+      }
+    end,
     config = function(_, opts)
       require("diffview").setup(opts)
     end,
